c9682f0f4a00ea29c3ab2157dd95b787256b1f1d
Mark Veerasingam <markveer70@gmail.com>
2023-10-04 14:27:42 +0100

    updated package name

--- src/main/java/ie/atu/y3_lab3_cicd/Y3Lab3CicdApplication.java
+++ src/main/java/ie/atu/y3_lab3_cicd/Y3Lab3CicdApplication.java
@@ -1,4 +1,4 @@
-package com.example.y3_lab3_cicd;
+package ie.atu.y3_lab3_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ie/atu/y3_lab3_cicd/Y3Lab3CicdApplicationTests.java
+++ src/test/java/ie/atu/y3_lab3_cicd/Y3Lab3CicdApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.y3_lab3_cicd;
+package ie.atu.y3_lab3_cicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

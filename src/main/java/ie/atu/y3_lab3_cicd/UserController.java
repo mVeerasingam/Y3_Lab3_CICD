e19364650ec601d6ed3ef9d8373eb3812223fcbb
Mark Veerasingam <markveer70@gmail.com>
2023-10-04 14:37:00 +0100

    made new branch for ex1, made UserController

---
+++ src/main/java/ie/atu/y3_lab3_cicd/UserController.java
@@ -0,0 +1,17 @@
+package ie.atu.y3_lab3_cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/registerHello/{name}/{email}")
+    public String getUser(@PathVariable String name,
+                          @PathVariable String email){
+
+        return email;
+    }
+
+}

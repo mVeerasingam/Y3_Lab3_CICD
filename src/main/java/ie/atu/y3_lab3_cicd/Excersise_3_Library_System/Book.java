bdc41e7f496a17190658fe06f87c05048b8a9a20
Mark Veerasingam <markveer70@gmail.com>
2023-10-07 00:35:11 +0100

    added lists to add and remove Book

--- src/main/java/ie/atu/y3_lab3_cicd/Excersise_3_Library_System/Book.java
+++ src/main/java/ie/atu/y3_lab3_cicd/Excersise_3_Library_System/Book.java
@@ -1,4 +1,17 @@
 package ie.atu.y3_lab3_cicd.Excersise_3_Library_System;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    private String title;
+    private String author;
+    private int isbn;
+    private int publishedYear;
 }

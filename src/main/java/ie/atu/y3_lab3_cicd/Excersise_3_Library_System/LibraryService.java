022428e6faaef33298f21662dfcbe054a03b5fad
Mark Veerasingam <markveer70@gmail.com>
2023-10-10 21:32:19 +0100

    updated methods

--- src/main/java/ie/atu/y3_lab3_cicd/Excersise_3_Library_System/LibraryService.java
+++ src/main/java/ie/atu/y3_lab3_cicd/Excersise_3_Library_System/LibraryService.java
@@ -1,14 +1,13 @@
 package ie.atu.y3_lab3_cicd.Excersise_3_Library_System;
+
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
-import java.util.Collections;
-
 // reference: https://shorturl.at/dhwHZ
 @Service
 public class LibraryService {
-    public static ArrayList<Book> bookArrayList = new ArrayList<Book>();
+    public static ArrayList<Book> bookArrayList = new ArrayList<>();
 
     public void addBook(Book book){
         bookArrayList.add(book);
@@ -16,9 +15,5 @@ public class LibraryService {
 
     public static ArrayList<Book> getBook() {
         return bookArrayList;
-    }
-
-    public static void viewArrayList() {
-        System.out.println(getBook());
     }
 }
